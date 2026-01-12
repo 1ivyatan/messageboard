7fc4fe4aa5ea1059e612e54e4067679627ab647a
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-01-12 15:09:50 +0200

    create form

--- client/src/components/MessageInput.tsx
+++ client/src/components/MessageInput.tsx
@@ -1,12 +1,17 @@
 export default function MessageInput () {
   const handleSubmit = async (e: any) => {
+    e.preventDefault();
     console.log(e)
   };
 
   return (
     <div className="border">
       <form action="#" onSubmit={handleSubmit}>
-        <input className="border" type="text" />
+        <input className="border" type="text" name="title" />
+        <br />
+        <textarea className="border" name="message"></textarea>
+        <br />
+        <button className="border" type="submit">Submit</button>
       </form>
     </div>
   );
