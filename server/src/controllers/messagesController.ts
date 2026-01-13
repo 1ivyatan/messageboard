f6cf9f1ec98f91b5db91095e0b8334a859dafc12
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-01-13 14:40:45 +0200

    send message to db

--- server/src/controllers/messagesController.ts
+++ server/src/controllers/messagesController.ts
@@ -1,14 +1,29 @@
 import express from "express";
+import { messageModel } from "../models/message";
 
 export function get(req: any, res: any): void {
-  res.json({ ok: true, timestamp: new Date().toISOString() });
+  res.json({ ok: true, timestamp: new Date().toISOString(), h: req.ip });
 }
 
-export function post(req: any, res: any): void {
-  const { title, message } = req.body ?? {};
+export async function post(req: any, res: any): void {
+  const { title, body } = req.body ?? {};
+  const clientIp = req.ip;
 
-  res.json({
+  const message = new messageModel({
     title: title,
-    message: message
+    body: body,
+    ip: req.ip,
+    date: Date.now()
   });
+
+  try {
+    await message.save();
+    res.status(200);
+    res.json({ message: "Message successfully sent" });
+  } catch (e: any)
+  {
+    console.log(e.message);
+    res.status(500);
+    res.json({ error: "Message was not sent" });
+  }
 }
