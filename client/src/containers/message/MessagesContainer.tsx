aafec2001345cdf4e1e2ef34563bed4da48d03eb
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-01-16 14:56:44 +0200

    adjust pagination

--- client/src/containers/message/MessagesContainer.tsx
+++ client/src/containers/message/MessagesContainer.tsx
@@ -5,51 +5,62 @@ import { Message } from "../../types/Message";
 export default function MessagesContainer() {
   enum Pagination {
     Prev = "firstId",
-    Next = "lastId"
+    Next = "lastId",
+    None = ""
   }
 
-  const [data, setData] = useState<Message[]>([]);
+  const [nextMessageId, setNextMessageId] = useState<String>("");
+  const [prevMessageId, setPrevMessageId] = useState<String>("");
+  const [messages, setMessages] = useState<Message[]>([]);
 
-  const [nextMessage, setNextMessage] = useState<String>("");
-  const [prevMessage, setPrevMessage] = useState<String>("");
+  const getData = (direction: Pagination) => {
+    let url = `${import.meta.env.VITE_API_BASE || "/api"}/messages`;
 
-  const getData = (messageId: String, direction: Pagination) => {
-
-    fetch(`${import.meta.env.VITE_API_BASE || "/api"}/messages${messageId !== "" ? "?" + direction + "=" + messageId : ""}`, {
-      method: "GET"
-    })
-    .then(async (response) => {
-      const items = await response.json();
-
-      setData(items);
-      setPrevMessage(items.at(0)._id);
-      setNextMessage(items.at(-1)._id);
-    });
-  };
-
-  useEffect(() => {
-    getData("", Pagination.Next);
-  }, []);
-
-  return <div className="border">
-    {
-      data.map((item: any) => {
-        return (
-          <MessageBox
-            key={"message_" + item._id}
-            id={item._id}
-            title={item.title}
-            body={item.body}
-            timestamp={item.timestamp}
-            votes={item.votes}
-          />
-        )
-      })
+    switch (direction) {
+      case Pagination.None: break;
+      case Pagination.Next: url += `?${Pagination.Next}=${nextMessageId}`; break;
+      case Pagination.Prev: url += `?${Pagination.Prev}=${prevMessageId}`; break;
     }
 
-    <div>
-      <button type="button" onClick={() => getData(prevMessage, Pagination.Prev)}>Previous</button>
-      <button type="button" onClick={() => getData(nextMessage, Pagination.Next)}>Next</button>
+    fetch(url)
+    .then(async (response) => {
+      const data = await response.json();
+      const items = data.data;
+      const meta = data.meta;
+
+      setNextMessageId(meta.next || "");
+      setPrevMessageId(meta.prev || "");
+      setMessages(items);
+    });
+  }
+
+  useEffect(() => {
+    getData(Pagination.None);
+  }, [])
+
+  return (
+
+    <div className="border">
+      <div>
+        {
+          messages.map((message: Message) => {
+            return (
+              <MessageBox
+                key={"message_" + message._id}
+                id={message._id}
+                title={message.title}
+                body={message.body}
+                timestamp={message.timestamp}
+                votes={message.votes}
+              />
+            );
+          })
+        }
+      </div>
+      <div>
+        <button disabled={prevMessageId === ""} type="button" onClick={(e: any) => getData(Pagination.Prev)}>Prev</button>
+        <button disabled={nextMessageId === ""} type="button" onClick={(e: any) => getData(Pagination.Next)}>Next</button>
+      </div>
     </div>
-  </div>;
+  );
 }
