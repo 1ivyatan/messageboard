685c2a1b2309f06575192ffda42d079fd2c206dc
Leons Pīgoznis <143333830+1ivyatan@users.noreply.github.com>
2026-01-19 16:23:49 +0200

    fetch with checks

--- client/src/stores/messagesStore.ts
+++ client/src/stores/messagesStore.ts
@@ -1,20 +1,29 @@
 import { create } from "zustand";
 import { Message } from "../types/Message";
 import { Meta } from "../types/Meta";
+import { Status } from "../types/Status";
 
 interface MessagesState {
   messages: Message[];
   meta: Meta;
+  status: Status;
   fetch: () => void;
 }
 
+const metaInitialState: Meta = {
+  next: null,
+  prev: null,
+};
+
 const useMessagesStore = create<MessagesState>()((set) => ({
   messages: [],
-  meta: {
-    next: null,
-    prev: null,
-  },
+  meta: metaInitialState,
+  status: Status.Loading,
   fetch: async () => {
+    set(() => ({
+      status: Status.Loading,
+    }));
+
     const url = `${import.meta.env.VITE_API_BASE || "/api"}/messages`;
     const response = await fetch(url);
 
@@ -24,9 +33,14 @@ const useMessagesStore = create<MessagesState>()((set) => ({
       set(() => ({
         messages: data.data,
         meta: data.meta,
+        status: Status.Success,
       }));
     } else {
-      console.log("err");
+      set(() => ({
+        messages: [],
+        meta: metaInitialState,
+        status: Status.Error,
+      }));
     }
   },
 }));
--- client/src/types/Status.ts
+++ client/src/types/Status.ts
@@ -1,4 +1,4 @@
-export enum MessagesStatus {
+export enum Status {
   Loading,
   Success,
   Error,
